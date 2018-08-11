32e85ff8c66263c87f43e07712732acf8b3f8663
极客慧 <1003669794@qq.com>
2018-08-12 02:06:18 +0800

    springboot集成Elastic-Job

---
+++ spring-boot-elastic-job-lite/src/main/java/com/jikeh/elasticjob/lite/SpringBootMain.java
@@ -0,0 +1,18 @@
+/**
+ * 极客慧 www.jikeh.cn
+ * 头条号：https://www.toutiao.com/c/user/52045142300/
+ */
+
+package com.jikeh.elasticjob.lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMain.class, args);
+    }
+
+}
