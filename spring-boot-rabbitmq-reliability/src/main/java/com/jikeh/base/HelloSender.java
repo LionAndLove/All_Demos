2edcfc908422aba061bb291e8688f5786eb19845
jikeh <1003669794@qq.com>
2018-09-23 01:03:28 +0800

    生产者消息确认几种场景的模拟

--- spring-boot-rabbitmq-reliability/src/main/java/com/jikeh/base/HelloSender.java
+++ spring-boot-rabbitmq-reliability/src/main/java/com/jikeh/base/HelloSender.java
@@ -1,6 +1,5 @@
 package com.jikeh.base;
 
-import com.jikeh.config.RabbitConfig;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.support.CorrelationData;
@@ -15,26 +14,41 @@ public class HelloSender implements RabbitTemplate.ConfirmCallback, RabbitTempla
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
-	public void send() {
+	public void send(String exchange, String routingKey) {
 		String context = "你好现在是 " + new Date();
 		System.out.println("send content = " + context);
 		this.rabbitTemplate.setMandatory(true);
 		this.rabbitTemplate.setConfirmCallback(this);
 		this.rabbitTemplate.setReturnCallback(this);
-		this.rabbitTemplate.convertAndSend(RabbitConfig.queueName, context);
+		this.rabbitTemplate.convertAndSend(exchange, routingKey, context);
 	}
 
+	/**
+	 * 确认后回调:
+	 * @param correlationData
+	 * @param ack
+	 * @param cause
+	 */
 	@Override
 	public void confirm(CorrelationData correlationData, boolean ack, String cause) {
 		if (!ack) {
-			System.out.println("send fail" + cause + correlationData.toString());
+			System.out.println("send ack fail, cause = " + cause);
 		} else {
-			System.out.println("send success");
+			System.out.println("send ack success");
 		}
 	}
 
+	/**
+	 * 失败后return回调：
+	 *
+	 * @param message
+	 * @param replyCode
+	 * @param replyText
+	 * @param exchange
+	 * @param routingKey
+	 */
 	@Override
 	public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
-		System.out.println("sender return success" + message.toString()+"==="+replyCode+"==="+replyText+"==="+exchange+"==="+routingKey);
+		System.out.println("send fail return-message = " + new String(message.getBody()) + ", replyCode: " + replyCode + ", replyText: " + replyText + ", exchange: " + exchange + ", routingKey: " + routingKey);
 	}
 }
--- spring-boot-rabbitmq-reliability/src/main/java/com/jikeh/controller/HelloController.java
+++ spring-boot-rabbitmq-reliability/src/main/java/com/jikeh/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.jikeh.controller;
 
 import com.jikeh.base.HelloSender;
+import com.jikeh.config.RabbitConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +16,44 @@ public class HelloController {
 	@RequestMapping("/base")
 	@ResponseBody
 	public String hello() {
-		helloSender.send();
+		helloSender.send(null, RabbitConfig.queueName);
 		return "success";
 	}
-	
+
+	/**
+	 * 下面模拟生产者发送状态的集中场景：
+	 */
+
+	//1、exchange, queue 都正确, confirm被回调, ack=true
+	@RequestMapping("/send1")
+	@ResponseBody
+	public String send1() {
+		helloSender.send(null, RabbitConfig.queueName);
+		return "success";
+	}
+
+	//2、exchange 错误, queue 正确, confirm被回调, ack=false
+	@RequestMapping("/send2")
+	@ResponseBody
+	public String send2() {
+		helloSender.send("fail-exchange", RabbitConfig.queueName);
+		return "success";
+	}
+
+	//3、exchange 正确, queue 错误, confirm被回调, ack=true; return被回调 replyText:NO_ROUTE
+	@RequestMapping("/send3")
+	@ResponseBody
+	public String send3() {
+		helloSender.send(null, "fail-queue");
+		return "success";
+	}
+
+	//4、exchange 错误, queue 错误, confirm被回调, ack=false
+	@RequestMapping("/send4")
+	@ResponseBody
+	public String send4() {
+		helloSender.send("fail-exchange", "fail-queue");
+		return "success";
+	}
+
 }
