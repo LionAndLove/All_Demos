7c70fe8f91c4796a05b8419289e69558f85d53e8
jikeh <1003669794@qq.com>
2018-09-23 03:34:43 +0800

    改下注释

--- spring-boot-rabbitmq-reliability/src/main/java/com/jikeh/base/HelloReceiver.java
+++ spring-boot-rabbitmq-reliability/src/main/java/com/jikeh/base/HelloReceiver.java
@@ -23,7 +23,7 @@ public class HelloReceiver {
             e.printStackTrace();
         }
         try {
-            //告诉服务器收到这条消息 已经被我消费了 可以在队列删掉 这样以后就不会再发了 否则消息服务器以为这条消息没处理掉 后续还会在发
+            //告诉服务器收到这条消息 已经被我消费了 可以在队列删掉；否则消息服务器以为这条消息没处理掉 后续还会在发
             channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
             System.out.println("receiver success = " + hello);
         } catch (Exception e) {
