51d2e27c6941a2ad585583e05413f9b87c85a006
hanwenteng <hanwenteng@corep.netease.com>
2019-08-06 10:50:12 +0800

    加些注释

--- MyDubbo/Dubbbo-ExtensionLoader/src/main/java/com/hadluo/dubbo/http/Http.java
+++ MyDubbo/Dubbbo-ExtensionLoader/src/main/java/com/hadluo/dubbo/http/Http.java
@@ -5,6 +5,12 @@ import com.hadluo.dubbo.extension.spi.SPI;
 
 @SPI("get")
 public interface Http {
+    /**
+     * 本来是要动态编译来实现动态代理, 在这里使用JDK来实现动态代理
+     * 区别：
+     *  动态编译：字节码技术
+     *  JDK：反射技术
+     */
     @Adaptive
     public void http();
 }
