42590f3956d29e563634f92bb70cc7bf43ae1d71
hanwenteng <hanwenteng@corep.netease.com>
2019-11-13 14:31:38 +0800

    123
    <footer>456
    
        feat(新特性),
        fix(修改问题),
        docs(文档修改),
        style(代码格式修改, 注意不是 css 修改),
        refactor(代码重构),
        test(测试),
        chore(其他修改, 比如构建流程, 依赖管理)
        影响的的范围
        提交描述

--- spring-boot-demo/src/main/java/com/jikeh/controller/HelloController.java
+++ spring-boot-demo/src/main/java/com/jikeh/controller/HelloController.java
@@ -14,7 +14,6 @@ public class HelloController {
 	@RequestMapping("/hello")
 	@ResponseBody
 	public String hello(String name) {
-		System.out.println("commit 测试");
 		logger.debug("Hello World");
 		return "hello, " + name;
 	}
